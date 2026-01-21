ccf087a9cbb3d71fee0fc3a45ded26aa8add962c
darvinh12 <arondon33@gmail.com>
2026-01-20 22:16:06 -0400

    ui: update address in footer for consistency

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -51,7 +51,7 @@ export function Footer() {
                             </li>
                             <li className="flex items-center gap-2">
                                 <MapPin className="h-4 w-4" />
-                                <span>Caracas, Venezuela</span>
+                                <span>Los Chaguaramos Caracas, CP 1050</span>
                             </li>
                         </ul>
                     </div>
